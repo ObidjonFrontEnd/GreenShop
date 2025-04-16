d56427cf29d941d5be403c3a0883cda803b033ef
ObidjonFrontEndn <lightline002@gmail.com>
2025-04-16 12:26:26 +0500

    3

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Join from './Join'
+import Contacts from './contacts'
+import FooterNav from './Nav'
+
+
+
+const Footer = () => {
+	return (
+		<div>
+			<Join />	
+			<Contacts/>
+			<FooterNav/>
+		</div>
+	)
+}
+
+export default Footer
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Menu from '../Components/Menu'
 import Navigation from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const MainLayout = () => {
 	return (
-		<div className='lg:max-w-[1216px]  pt-[25px] pb-[10px] md:px-[50px] 2xl:px-0 mx-auto overflow-x-hidden'>
+		<div className='lg:max-w-[1216px] pt-[25px] pb-[10px] md:px-[50px] 2xl:px-0 mx-auto overflow-x-hidden'>
 			<Navigation />
 			<Menu />
-			{<Outlet />}
+			<Outlet />
+			<Footer/>
 		</div>
 	)
 }
