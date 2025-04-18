d70d64618797b32a1301a03177b7edb7511caea9
ObidjonFrontEndn <lightline002@gmail.com>
2025-04-18 18:58:30 +0500

    4.1

--- src/hooks/Get.js
+++ src/hooks/Get.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import apiAxios from '../Api/Api'
+import apiAxios from '../api/api.jsx'
 
 const useGetApi = (url = {}) => {
 	const axiosData = async () => {
